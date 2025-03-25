e8e06370769614ffa55c5400d36b48420b81dc92
Yaryna Romaniuk <yaryna.romaniuk.pm.2023@lpnu.ua>
2025-03-25 20:57:31 +0200

    sections

---
+++ src/app/main-section/black-section/black-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-black-section',
+  imports: [],
+  templateUrl: './black-section.component.html',
+  styleUrl: './black-section.component.scss'
+})
+export class BlackSectionComponent {
+
+}
---
+++ src/app/name-and-photo/name-and-photo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-name-and-photo',
+  imports: [],
+  templateUrl: './name-and-photo.component.html',
+  styleUrl: './name-and-photo.component.scss'
+})
+export class NameAndPhotoComponent {
+
+}
